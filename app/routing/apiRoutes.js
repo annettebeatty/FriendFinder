568c18130cfda8f27b97dc815bc3eca7fc5e7d5c
annette.beatty@gmail.com <annette.beatty@gmail.com>
2018-08-11 01:14:26 -0700

    Post working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/survey", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
@@ -29,14 +29,7 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
-  app.post("/api/survey", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-
-    // This code will need to push data into the friends array
+  app.post("/api/friends", function(req, res) {
     friendsData.push(req.body);
   });
-
 };
