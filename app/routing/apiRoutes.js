9de223ec8562c12560479ab0c4a1bff2cf811522
annette.beatty@gmail.com <annette.beatty@gmail.com>
2018-08-11 16:54:39 -0700

    Added comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on friends
 // ===============================================================================
 
 var friendsData = require("../data/friends");
@@ -27,7 +27,7 @@ module.exports = function(app) {
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // Then the server saves the data to the friendsData array)
   // ---------------------------------------------------------------------------
   app.post("/api/friends", function(req, res) {
     friendsData.push(req.body);
